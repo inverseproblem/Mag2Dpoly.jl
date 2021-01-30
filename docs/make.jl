fa98407af1d4f3da4eebe3d6d5bc36a93537f8f5
Andrea Zunino <>
2021-01-30 18:21:56 +0100

    modified:   make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,14 @@
 
 using Documenter, Mag2Dpoly
 
-makedocs(sitename="Mag2Dpoly.jl")
-#         modules = [Mag2Dpoly],
-#         authors = "Andrea Zunino, Alessandro Ghirotto",
-#         format = Documenter.HTML(prettyurls=get(ENV,"CI",nothing)=="true"),
-#         pages = [
-#             "Home" => "index.md",
-#        ]
-
+makedocs(sitename="Mag2Dpoly.jl",
+         modules = [Mag2Dpoly],
+         authors = "Andrea Zunino, Alessandro Ghirotto",
+         format = Documenter.HTML(prettyurls=get(ENV,"CI",nothing)=="true"),
+         pages = [
+             "Home" => "index.md",
+         ]
+         )
 
 deploydocs(
     repo="github.com/inverseproblem/Mag2Dpoly.jl.git",
